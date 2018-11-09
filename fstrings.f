77e9425c84f5a762ca63d8d65f269aeedb996505
BoneKing <mahonea@clarkson.edu>
2018-11-08 23:45:28 -0500

    weird errors on while

--- fstrings.f
+++ fstrings.f
@@ -1,19 +1,30 @@
       program fstrings
-      INTEGER I, B=1
-      CHARACTER A(50) 
-      CALL Readfstring
-      CALL Writefstring 
+      INTEGER I, I2, B
+      CHARACTER A(50), reads 
+      I=1
+      I2=1
+      B=1
+      CALL Readfstring()
+ 10   continue 
+      I=1
+      CALL Writefstring()
+ 20   continue
+      I=1
       stop 
       end
       SUBROUTINE Readfstring   
-  10    continue
-        DO 10 A .NE. '\0'
-          read(*,*) A(I)
+        do while reads .NE. '~'
+          A(I)=reads
           I=I+1
-            
-      end subroutine  
+          goto 10
+          end do
+        return
+      end subroutine 
       SUBROUTINE Writefstring
-        DO 20 B .LE. I
+        do while A(I) .NE. '~' 
           write(*,*) A(I)
-  20    CONTINUE
-      end subroutine
+          I2=I2+1
+          goto 20
+          end do
+        return
+      end subroutine 
